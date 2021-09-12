723560bd12750ad66710579ad8d041a0b0446a57
Taraneh Saleh <taranehsaleh@Taranehs-MacBook-Pro.local>
2021-09-12 16:42:05 -0400

    started js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//AP'S STATE (vaiables that change)
+let board;
+let playerTurn;
+let Winner;
+
+// CASHED ELEMENT REFERENCES
+let gameBoard = document.getElementById('mancala-board')
+let reStartButton = documetn.getElementById('restart-button')
+
+//EVENT LISTENERS
+
+
+// FUNCTIONS
+
+
