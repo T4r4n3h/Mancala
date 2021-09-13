d2782089d450e144bf64305d30f693c13e7dd1b1
Taraneh Saleh <taranehsaleh@Taranehs-MacBook-Pro.local>
2021-09-13 14:52:10 -0400

    jsut wrote to log each pit with corespoding marbles

--- js/script.js
+++ js/script.js
@@ -4,22 +4,25 @@ let playerTurn;
 let Winner;
 
 // CASHED ELEMENT REFERENCES
-let gameBoard = document.getElementById('mancala-board')
-let reStartButton = document.getElementById('restart-button')
+let mancalaBoard = document.getElementById('mancala-board')
+let restartButton = document.getElementById('restart-button')
+// let pits = document.querySelectorAll('.pit')
 
 //EVENT LISTENERS
-
+mancalaBoard.addEventListener('click', handleClick)
 
 // FUNCTIONS
 function init(){
     playerTurn = 1
-    board = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]
+    pits = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]
 }
+// write out a code that allows the ideces of the board corespond with doc.
 
-if(board[index]!==6 && board[index] !==13){
-    board.forEach(function(index){
-        addEventListener
-    })
-
-    
+function handleClick(evt){
+    let i = 0;
+    let pitNum = parseInt(evt.target.id);
+    console.log(pitNum, pits[i])
 }
+init()
+pits =  pits = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]
+for(let i = 0; i < pits.length; i++) 
