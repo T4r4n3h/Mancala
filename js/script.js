c9911010020e30a5346491f519b3f4750b68d21e
Taraneh Saleh <taranehsaleh@Taranehs-MacBook-Pro.local>
2021-09-13 09:59:56 -0400

    added to pseudocode, added a restart button to html and a few lines of js

--- js/script.js
+++ js/script.js
@@ -5,11 +5,21 @@ let Winner;
 
 // CASHED ELEMENT REFERENCES
 let gameBoard = document.getElementById('mancala-board')
-let reStartButton = documetn.getElementById('restart-button')
+let reStartButton = document.getElementById('restart-button')
 
 //EVENT LISTENERS
 
 
 // FUNCTIONS
+function init(){
+    playerTurn = 1
+    board = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]
+}
 
+if(board[index]!==6 && board[index] !==13){
+    board.forEach(function(index){
+        addEventListener
+    })
 
+    
+}
