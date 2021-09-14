91f72516fbe179e982c6cb947b76d1cd33f24370
Taraneh Saleh <taranehsaleh@Taranehs-MacBook-Pro.local>
2021-09-14 12:39:50 -0400

    add render function to handle event

--- js/script.js
+++ js/script.js
@@ -26,25 +26,33 @@ function init(){
     pits = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0]
 }
 
+function render(){
+   pits.forEach(function(pit, idx ){
+       document.getElementById(idx).textContent = pit;
+   })
+}
 
 function handleClick(evt){
     let pitIdx= parseInt(evt.target.id);
     console.log(`at index # ${pitIdx}:  ${pits[pitIdx]} marbles`)
    // need a function to distribute marbles if the pit is not empty 
-   if(pitIdx=== 0) return
+   if(pitIdx=== 6 || pitIdx === 13) return;
    // playerA(1) can not click on pits 6-13
-   if(playerTurn ===1 && pitIdx > 6) return
+   if(playerTurn ===1 && pitIdx > 6) return;
    //playerB(-1) can not click on 13 or 0-6
    if(playerTurn < 0 && (pits[pitIdx] <= 6 || pit[Idx] == 13)) return;
     // distributeMarble()
     distributeMarble(pitIdx)
+
+    render()
+    console.log(pits)
  }
 
 // writing a function to take the number of marble in the bowl and sitribute them while the number is greater thatn 0. incrementing the marbles as well as the whole and decrementing the original marbles 
 
 function distributeMarble(i){
     let numMarbles = pits[i];
-   pits[i]= 0
+    pits[i]= 0
     while (numMarbles > 0){
         
        numMarbles --
