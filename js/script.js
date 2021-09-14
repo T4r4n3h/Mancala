8f43fb19a954de810bfe22713de5c8fa6cac9333
Taraneh Saleh <taranehsaleh@Taranehs-MacBook-Pro.local>
2021-09-14 06:06:51 -0400

    add if statements to handle click

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
+
+
+
 //AP'S STATE (vaiables that change)
+
 let board;
 let playerTurn;
 let Winner;
@@ -9,11 +13,13 @@ init()
 // CASHED ELEMENT REFERENCES
 let mancalaBoard = document.getElementById('mancala-board')
 let restartButton = document.getElementById('restart-button')
-// let pits = document.querySelectorAll('.pit')
+
 
 //EVENT LISTENERS
 mancalaBoard.addEventListener('click', handleClick)
+restartButton.addEventListener('click', function(){
 
+})
 // FUNCTIONS
 function init(){
     playerTurn = 1
@@ -23,18 +29,24 @@ function init(){
 
 function handleClick(evt){
     let pitIdx= parseInt(evt.target.id);
-    console.log(`whole # ${pitIdx},has ${pits[pitIdx]} marbles`)
-   // need a function to distribute marbles 
+    console.log(`at index # ${pitIdx}:  ${pits[pitIdx]} marbles`)
+   // need a function to distribute marbles if the pit is not empty 
+   if(pits[pitIdx]=== 0) return;
+   // playerA(1) can not click on pits 6-13
+   if(playerTurn > 0 && pits[pitIdx] > 6) return;
+   //playerB(-1) can not click on 13 or 0-6
+   if(playerTurn < 0 && (pits[pitIdx] <= 6 || pit[Idx] == 13)) return;
+    distributeMarble()
  }
 
 // writing a function to take the number of marble in the bowl and sitribute them while the number is greater thatn 0. incrementing the marbles as well as the whole and decrementing the original marbles 
 
 function distributeMarble(){
-    let numMarbles = pits[pitIdx];
-    pits[pitIdx]= 0;
-    pitIdx ++;
-    while (numMarbles > 0) {
-        pits
+    let numMarbles = pits[i];
+   
+    while (numMarbles > 0){
+        console.log(numMarbles)
     }
-numMarbles --
+    
+ 
 }
